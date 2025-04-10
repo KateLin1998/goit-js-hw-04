271d68de174e410fe8924e0721f934615953a630
Kateryna <katarina144144@gmail.com>
2025-04-10 23:08:51 +0300

    HW done

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,23 @@
-const defaultSettings = {
-  theme: "light",
-  public: true,
-  withPassword: false,
-  minNumberOfQuestions: 10,
-  timePerQuestion: 60,
-};
-const overrideSettings = {
-  public: false,
-  withPassword: true,
-  timePerQuestion: 30,
-};
+function isEnoughCapacity(products, containerSize) {
+  const totalProducts = Object.values(products).reduce(function(acc, value) {
+    return acc + value;
+  }, 0);
 
-const finalSettings = {...defaultSettings, ...overrideSettings};
+  return totalProducts <= containerSize;
+}
 
-console.log(finalSettings);
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
